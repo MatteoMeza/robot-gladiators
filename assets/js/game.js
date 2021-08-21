70566d5737c43af0807ebd4c252ec821e9305799
Matteo Meza <matteomeza@gmail.com>
2021-08-20 20:05:12 -0700

    add basic game play

--- assets/js/game.js
+++ assets/js/game.js
@@ -24,6 +24,20 @@ var fight = function() {
     console.log(
         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
+    // check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+    // check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
 };
 
 
